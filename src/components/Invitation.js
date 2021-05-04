3fc92d53160c93027c95b6b3b14371476f9e28bc
MOBOLA MORONTO <t2wycemo@gmail.com>
2021-05-04 16:23:45 +0000

    Automatically backed up by Learn

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -3,20 +3,12 @@ import React from 'react';
 class Invitation extends React.Component {
   
   render() {
-    const invDefault = <h1>'You\'ve been invited!'</h1>;
-    const invCust = React.Children.map(this.props.children, child => {
-      rturn React.cloneElement(child, {
-        invite: child.props.message === invDefault
-      });
-    });
-    
     return (
       <div>
         <h1>You've been invited!</h1>
         {this.props.children}
       </div>
-    );
-      
+    )
   }
 }
 
